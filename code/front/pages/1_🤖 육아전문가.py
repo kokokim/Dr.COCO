1382eebbe905ed012a882de73cbcd6288d5be59e
kimseonah <seona918@gmail.com>
2024-02-26 20:53:15 +0900

    revise ==

--- code/front/pages/1_🤖 육아전문가.py
+++ code/front/pages/1_🤖 육아전문가.py
@@ -26,8 +26,8 @@ import streamlit as st
 from openai import OpenAI
 from PIL import Image # 위에서 선언 후 사용해야한다.
 
-os.environ['OPENAI_API_KEY'] == st.secrets["OPENAI_API_KEY"]
-os.environ["TOKENIZERS_PARALLELISM"] == st.secrets["TOKENIZERS_PARALLELISM"]
+os.environ['OPENAI_API_KEY'] = st.secrets["OPENAI_API_KEY"]
+os.environ["TOKENIZERS_PARALLELISM"] = st.secrets["TOKENIZERS_PARALLELISM"]
 
 client = OpenAI()
 
