b4e2f9881a1bf8d4428e3a1d2bc153c4180a7dd3
kimseonah <seona918@gmail.com>
2024-02-27 23:01:46 +0900

    change css

--- code/front/pages/2_🍼 제품추천가.py
+++ code/front/pages/2_🍼 제품추천가.py
@@ -25,34 +25,40 @@ os.environ["TOKENIZERS_PARALLELISM"] = st.secrets["TOKENIZERS_PARALLELISM"]
 script_dir = os.path.dirname(__file__)
 style_path = os.path.join(script_dir, "../style.css")
 
-with open(style_path) as css:
-    # st.set_page_config(page_title="COCO PRODUCTBOT")
-    st.title("COCO PRODUCTBOT")
-    st.text("안녕하세요. 코코 제품추천챗봇입니다. 원하시는 질문을 입력해주세요.")
-    # st.text("현재 추천가능한 상품은 젖병, 유모차, 로션 입니다.")
-    st.divider()
-        
-    if "page2_messages" not in st.session_state: # Initialize the chat message history
-        st.session_state.page2_messages = [
-            {"role": "assistant", 
-            "content": """안녕하세요. 코코박사입니다. 무엇을 찾으시나요?"""}
-        ]
+st.set_page_config(page_title="COCO PRODUCTBOT", page_icon="🤖")
 
-    # 사용자 입력 프롬프트 및 메시지 기록 표시
-    if prompt := st.chat_input("답변을 입력해주세요"): # Prompt for user input and save to chat history
-        st.session_state.page2_messages.append({"role": "user", "content": prompt}) #prompt에 답변 저장
-            
-    for message in st.session_state.page2_messages: # Display the prior chat messages
-        with st.chat_message(message["role"]):
-            st.write(message["content"])
-            
-            
-    # 쿼리를 채팅 엔진에 전달하고 응답을 표시
-    if st.session_state.page2_messages[-1]["role"] != "assistant":
-        with st.chat_message("assistant"):
-            with st.spinner("Thinking..."):
-                response = product.main(prompt)
-                # response="ahahaha"
-                st.write(response)
-                message = {"role": "assistant", "content": response}
-                st.session_state.page2_messages.append(message) # Add response to message history
+with open(style_path) as css:
+    st.markdown (f'<style>{css.read()}</style>', unsafe_allow_html=True)
+
+st.markdown(f"""
+            <div class="producttitle">COCO PRODUCTBOT🤖</div>
+            <p class="producttext"> 코코 제품추천챗봇입니다. 원하시는 질문을 입력해주세요.</p>
+            """,
+            unsafe_allow_html=True
+            )
+# st.text("현재 추천가능한 상품은 젖병, 유모차, 로션 입니다.")
+    
+if "page2_messages" not in st.session_state: # Initialize the chat message history
+    st.session_state.page2_messages = [
+        {"role": "assistant", 
+        "content": """안녕하세요. 코코박사입니다. 무엇을 찾으시나요?"""}
+    ]
+
+# 사용자 입력 프롬프트 및 메시지 기록 표시
+if prompt := st.chat_input("질문을 입력해주세요"): # Prompt for user input and save to chat history
+    st.session_state.page2_messages.append({"role": "user", "content": prompt}) #prompt에 답변 저장
+        
+for message in st.session_state.page2_messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+        
+        
+# 쿼리를 채팅 엔진에 전달하고 응답을 표시
+if st.session_state.page2_messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = product.main(prompt)
+            # response="ahahaha"
+            st.write(response)
+            message = {"role": "assistant", "content": response}
+            st.session_state.page2_messages.append(message) # Add response to message history
--- code/model/sleep.py
+++ code/model/sleep.py
@@ -1,5 +1,3 @@
-# 카테고리별 vectordb 생성 후 불러오기
-
 import os
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
@@ -12,10 +10,10 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chains.conversation.memory import ConversationSummaryMemory                                        
 import tiktoken
+import pandas as pd
 
-os.environ['OPENAI_API_KEY'] = st.secrets["OPENAI_API_KEY"]
-os.environ["TOKENIZERS_PARALLELISM"] = st.secrets["TOKENIZERS_PARALLELISM"]
-
+os.environ['OPENAI_API_KEY'] = "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"
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 # baby.py 파일의 절대 경로
 current_file_path = os.path.abspath(__file__)
@@ -35,7 +33,6 @@ raw_data_dir = os.path.join(root_dir, 'data', 'raw')
 # 'data/vectordb/baby' 폴더로의 상대 경로
 folder_path = os.path.join(root_dir, 'data', 'vectordb', 'baby')
 
-
 documents_dicts={
     'raisebaby':[],
     'breastfeeding':[],
@@ -44,6 +41,43 @@ documents_dicts={
     'supportsystem':[],
     'babyfood':[]
 }
+
+data={
+    'user_id': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    'num_daysleep': [4, 1, 2, 3, 1, 3, 2, 2, 1, 2, 3, 3, 2],
+    'time_daysleep': [180, 155, 120, 160, 132, 111, 143, 127, 88, 169, 175, 140, 176],
+    'time_nightsleep': [900, 840, 863, 788, 801, 893, 867, 916, 925, 848, 800, 833, 888],
+    'time_totalsleep': [1080, 995, 983, 948, 933, 1004, 1010, 1043, 1013, 1017, 975, 973, 1064],
+    'date': ['2024-02-16', '2024-02-17', '2024-02-18', '2024-02-19', '2024-02-20', '2024-02-21', '2024-02-22', '2024-02-23', '2024-02-24', '2024-02-25', '2024-02-26', '2024-02-27', '2024-02-28'],
+    'child_age': [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    'child_name': ['동동이']*13
+}
+
+df=pd.DataFrame(data)
+
+#가장 최근의 수면 데이터
+latest_data=df.sort_values(by='date', ascending=False).iloc[0]
+
+def format_minutes(minutes):
+    hours=minutes//60
+    minutes=minutes%60
+    return f"{hours}시간 {minutes}분"
+
+# alert 메시지 생성
+formatted_day_sleep = format_minutes(latest_data['time_daysleep'])
+formatted_night_sleep = format_minutes(latest_data['time_nightsleep'])
+formatted_total_sleep = format_minutes(latest_data['time_totalsleep'])
+
+alert_message = (
+    f"어제 {latest_data['child_name']}의 낮잠 시간은 {formatted_day_sleep}, "
+    f"낮잠 횟수는 {latest_data['num_daysleep']}회, 밤잠 시간은 {formatted_night_sleep}으로 "
+    f"총 수면 시간은 {formatted_total_sleep}입니다."
+)
+
+# 챗봇에게 질문 구성
+chatbot_question = (
+    f"나이가 {latest_data['child_age']} 개월인 {latest_data['child_name']}의 {alert_message} 수면 시간 적당해?"
+)
 
 # 문서 로드 및 벡터 DB 생성
 def load_documents_and_create_vector_db(folder_path, documents_dict):
@@ -57,13 +91,15 @@ def load_documents_and_create_vector_db(folder_path, documents_dict):
         for category, docs in documents_dicts.items():
             vector_db = FAISS.from_documents(docs, ko)
             vector_db.save_local(f"{folder_path}/{category}_faiss_index")
-
-
+    
+            
 # 카테고리 선택 및 DB 로드
-def select_category_and_load_db(category):
-    selected_db_path = f'{folder_path}/{category}_faiss_index'
+def select_category_and_load_db():
+    selected_db_path = f'{folder_path}/basicparenting_faiss_index'
     selected_vector_db = FAISS.load_local(selected_db_path, ko)
     return selected_vector_db.as_retriever(search_kwargs={'k': 3})
+
+
 
 # 챗봇 모델 및 체인 설정
 def setup_chat_model_and_chain(category_retriever):
@@ -81,27 +117,21 @@ def setup_chat_model_and_chain(category_retriever):
     )
     return chain
 
-    
 # 사용자 질문에 대한 응답 생성
-def chat_start(query, chain):
-    if query == "그만":
-        return "채팅을 종료합니다."
-    response = chain(query)
-    
+def chat_start(chain):
+    response=chain(chatbot_question)
+    print("이잉", response['answer'])
     return response['answer']
 
 
-# 메인 함수: 사용자가 선택한 카테고리에 따라 챗봇을 설정하고 실행
-def main(category, query):
-    # baby_info_save
+def main():
     load_documents_and_create_vector_db(folder_path, documents_dict)
-    category_retriever = select_category_and_load_db(category)
-    chain = setup_chat_model_and_chain(category_retriever)
-    response = chat_start(query, chain)
+    sleep_retriever = select_category_and_load_db()
+    chain = setup_chat_model_and_chain(sleep_retriever)
+    response = chat_start(chain)
     return response
-    
-    
-# 전역 변수 및 인스턴스 생성
+
+
 documents_dict = {
     'm육아대백과_신생아키우기': 'raisebaby',
     'm육아대백과_수유': 'breastfeeding',
@@ -118,7 +148,7 @@ def titoken_len(text):
 
 splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50, length_function=titoken_len)
 ko = HuggingFaceEmbeddings(model_name='jhgan/ko-sbert-nli', model_kwargs={'device': 'cpu'}, encode_kwargs={'normalize_embeddings': True})
-chat_model = ChatOpenAI(temperature=0.1, model_name="gpt-3.5-turbo")
+chat_model = ChatOpenAI(temperature=0.1, model_name="gpt-4-turbo-preview")
 memory = ConversationSummaryMemory(
     llm=chat_model,
     max_token_limit=80,
@@ -127,18 +157,6 @@ memory = ConversationSummaryMemory(
     ai_prefix='### AI',
     output_key='answer',
     return_messages=True
-)
-memory.save_context(
-    {"input": "아이의 이름은 김동동이야."},
-    {"answer": "당신의 아이의 이름은 김동동이군요! 예쁜 이름입니다."}
-)
-memory.save_context(
-    {"input" : "아이의 성별은 남자아이야."},
-    {"answer":"당신의 아이의 성별은 남자아이이군요!"}
-)
-memory.save_context(
-    {"input":"아이가 태어난 날은 2023년 12월 25일이야."},
-    {"answer" : "당신의 아이는 2023년 12월 25일에 태어났군요. 지금은 2024년 2월 25일로 아기는 3개월이 되었어요."}
 )
 
 template="""
